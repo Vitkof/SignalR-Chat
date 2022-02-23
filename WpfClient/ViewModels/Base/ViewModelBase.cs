3efdfb70fe34cf7cbd1f1c09efe254b727a66944
Vitkof <71389732+Vitkof@users.noreply.github.com>
2022-02-23 20:16:16 +0300

    WPF Client empty project with start preparations

---
+++ WpfClient/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfClient.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _Disposed;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        //разрешить кольцевые изменения свойств
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            else
+            {
+                field = value;
+                OnPropertyChanged(PropertyName);
+                return true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+
+        }
+    }
+}
---
+++ WpfClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfClient.ViewModels.Base;
+
+
+namespace WpfClient.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private string _displayHeader = "WPF Client";
+
+        public string DisplayHeader
+        {
+            get => _displayHeader;
+            set => Set(ref _displayHeader, value);
+        }
+    }
+}
