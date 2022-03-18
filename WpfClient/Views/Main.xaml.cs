7ef3994ef529ddb3927a83d11362e5be5f2e8beb
Vitkof <71389732+Vitkof@users.noreply.github.com>
2022-03-18 21:39:58 +0300

    Wpf: UI main window

--- WpfClient/Views/Main.xaml.cs
+++ WpfClient/Views/Main.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfClient.ViewModels;
 
 namespace WpfClient.Views
 {
@@ -23,6 +24,7 @@ namespace WpfClient.Views
         public Main()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
